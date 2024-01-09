e7189d8868ab8bc8775ece7e47525b3bbba0d061
José A Rodríguez <roorjoan@gmail.com>
2024-01-09 10:36:23 -0600

    proyecto terminado
    ref, props, emits, v-for, v-if, v-else, computed, @click, watch, localStorage, findIndex, filter, reduce

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,76 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, watch } from "vue";
+import { db } from "./data/guitarras";
+import Guitarra from './components/Guitarra.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+const guitarras = ref([]);
+const carrito = ref([]);
+const guitarra = ref({});
+
+watch(carrito, () => {
+  guardarLocalStorage()
+}, {
+  deep: true
+});
+
+onMounted(() => {
+  guitarras.value = db;
+  guitarra.value = db[3];
+
+  const carritoStorage = localStorage.getItem('carrito');
+  if (carritoStorage) {
+    carrito.value = JSON.parse(carritoStorage);
+  }
+});
+
+const agregarCarrito = (guitarra) => {
+  const existeCarrito = carrito.value.findIndex(element => element.id === guitarra.id);
+
+  if (existeCarrito >= 0) {
+    if (carrito.value[existeCarrito].cantidad >= 5) return;
+    carrito.value[existeCarrito].cantidad++;
+  } else {
+    guitarra.cantidad = 1;
+    carrito.value.push(guitarra);
+  }
+}
+
+const vaciarCarrito = () => carrito.value = [];
+
+const decrementarCantidad = (id) => {
+  const index = carrito.value.findIndex(element => element.id === id);
+  if (carrito.value[index].cantidad <= 1) return;
+  carrito.value[index].cantidad--;
+}
+
+const incrementarCantidad = (id) => {
+  const index = carrito.value.findIndex(element => element.id === id);
+  if (carrito.value[index].cantidad >= 5) return;
+  carrito.value[index].cantidad++;
+}
+
+const eliminarGuitarra = (id) => {
+  carrito.value = carrito.value.filter(guitarra => guitarra.id !== id);
+}
+
+const guardarLocalStorage = () => localStorage.setItem('carrito', JSON.stringify(carrito.value));
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <Header :carrito="carrito" :guitarra="guitarra" @vaciar-carrito="vaciarCarrito"
+    @decrementar-cantidad="decrementarCantidad" @incrementar-cantidad="incrementarCantidad"
+    @agregar-carrito="agregarCarrito" @eliminar-guitarra="eliminarGuitarra" />
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  <main class="container-xl mt-5">
+    <h2 class="text-center">Nuestra Colección</h2>
+
+    <div class="row mt-5">
+      <Guitarra v-for="guitarra in guitarras" :key="guitarra.id" :guitarra="guitarra" @agregar-carrito="agregarCarrito" />
+    </div>
+  </main>
+
+  <Footer />
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  carrito: {
+    type: Array,
+    required: true
+  },
+  guitarra: {
+    type: Object,
+    required: true
+  }
+});
+
+defineEmits(['vaciar-carrito', 'decrementar-cantidad', 'incrementar-cantidad', 'agregar-carrito', 'eliminar-guitarra']);
+
+const totalPagar = computed(() => {
+  return props.carrito.reduce((total, guitarra) => total + (guitarra.precio * guitarra.cantidad), 0);
+});
+
+</script>
+
+<template>
+  <header class="py-5 header">
+    <div class="container-xl">
+      <div class="row justify-content-center justify-content-md-between">
+        <div class="col-8 col-md-3">
+          <a href="index.html">
+            <img class="img-fluid" src="/img/logo.svg" alt="imagen logo">
+          </a>
+        </div>
+        <nav class="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+          <div class="carrito">
+            <img class="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
+
+            <div id="carrito" class="bg-white p-3">
+
+              <p v-if="carrito.length === 0" class="text-center m-0">El carrito esta vacio</p>
+
+              <div v-else>
+                <table class="w-100 table">
+                  <thead>
+                    <tr>
+                      <th>Imagen</th>
+                      <th>Nombre</th>
+                      <th>Precio</th>
+                      <th>Cantidad</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="guitarra in carrito" :key="guitarra.id">
+                      <td>
+                        <img class="img-fluid" :src="'/img/' + guitarra.imagen + '.jpg'" alt="imagen guitarra">
+                      </td>
+                      <td>{{ guitarra.nombre }}</td>
+                      <td class="fw-bold">
+                        ${{ guitarra.precio }}
+                      </td>
+                      <td class="flex align-items-start gap-4">
+                        <button @click="$emit('decrementar-cantidad', guitarra.id)" type="button" class="btn btn-dark">
+                          -
+                        </button>
+                        {{ guitarra.cantidad }}
+                        <button @click="$emit('incrementar-cantidad', guitarra.id)" type="button" class="btn btn-dark">
+                          +
+                        </button>
+                      </td>
+                      <td>
+                        <button @click="$emit('eliminar-guitarra', guitarra.id)" class="btn btn-danger" type="button">
+                          X
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+
+                <p class="text-end">Total pagar: <span class="fw-bold">${{ totalPagar }}</span></p>
+                <button @click="$emit('vaciar-carrito')" class="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
+              </div>
+
+            </div>
+          </div>
+        </nav>
+      </div><!--.row-->
+
+      <div class="row mt-5">
+        <div class="col-md-6 text-center text-md-start pt-5">
+          <h1 class="display-2 fw-bold">Modelo {{ guitarra.nombre }}</h1>
+          <p class="mt-5 fs-5 text-white">{{ guitarra.descripcion }}</p>
+          <p class="text-primary fs-1 fw-black">${{ guitarra.precio }}</p>
+          <button @click="$emit('agregar-carrito', guitarra)" type="button"
+            class="btn fs-4 bg-primary text-white py-2 px-5">Agregar al Carrito</button>
+        </div>
+      </div>
+    </div>
+
+    <img class="header-guitarra" src="/img/header_guitarra.png" alt="imagen header">
+  </header>
+</template>
